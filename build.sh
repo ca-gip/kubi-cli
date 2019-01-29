0e52cdc09073cf306352dc52b8796af134badd20
Emmanuel Fortin <emmanuel.fortin@ca-silca.fr>
2019-01-29 18:16:33 +0100

    Add windows build Add windows compilation

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-/usr/bin/go build -o bin/kubi
+GOOS=linux go build -o ./bin/kubi kubi-cli.go
+GOOS=windows GOARCH=386 go build -o ./bin/kubi.exe
--- kubi-cli.go
+++ kubi-cli.go
@@ -28,6 +28,7 @@ func main() {
 
 	kubiUrl := flag.String("kubi-url", "", "Url to kubi server (ex: https://<kubi-ip>:<kubi-port>")
 	generateConfig := flag.Bool("generate-config", false, "Generate a config in ~/.kube/config")
+	insecure := flag.Bool("insecure", false, "Skip TLS verification")
 	username := flag.String("username", "", "Ldap username ( not dn )")
 	flag.Parse()
 
@@ -59,31 +60,44 @@ func main() {
 	check(err)
 	ca := body
 
-	// Get the SystemCertPool, continue with an empty pool on error
-	rootCAs, _ := x509.SystemCertPool()
-	if rootCAs == nil {
-		rootCAs = x509.NewCertPool()
-	}
-
-	if ok := rootCAs.AppendCertsFromPEM(ca); !ok {
-		log.Println("No certs appended, using system certs only")
-	}
-	// Trust the augmented cert pool in our client
-	tlsConfig := &tls.Config{
-		InsecureSkipVerify: false,
-		RootCAs:            rootCAs,
-	}
-
-	transport := &http.Transport{
-		TLSClientConfig: tlsConfig,
-	}
-
-	secureClient := http.Client{}
-	secureClient.Transport = transport
 	req, err := http.NewRequest(http.MethodGet, *kubiUrl+"/config", nil)
 	req.SetBasicAuth(*username, string(bytePassword))
-	resp, err := secureClient.Do(req)
+
+	var resp *http.Response
+
+	if *insecure {
+		transport := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		insecureClient := http.Client{}
+		insecureClient.Transport = transport
+		resp, err = insecureClient.Do(req)
+	} else {
+		secureClient := http.Client{}
+
+		// Get the SystemCertPool, continue with an empty pool on error
+		rootCAs, _ := x509.SystemCertPool()
+		if rootCAs == nil {
+			rootCAs = x509.NewCertPool()
+		}
+
+		if ok := rootCAs.AppendCertsFromPEM(ca); !ok {
+			log.Println("No certs appended, using system certs only")
+		}
+
+		// Trust the augmented cert pool in our client
+		tlsConfig := &tls.Config{
+			InsecureSkipVerify: false,
+			RootCAs:            rootCAs,
+		}
+		transport := &http.Transport{
+			TLSClientConfig: tlsConfig,
+		}
+		secureClient.Transport = transport
+		resp, err = secureClient.Do(req)
+	}
 	check(err)
+
 	tokenbody, err := ioutil.ReadAll(resp.Body)
 	if resp.StatusCode != http.StatusCreated {
 		fmt.Printf("Error http %d during authentication\n", resp.StatusCode)
