8c5bbbc4b67565307406dbbd7b444ddbbf0ac877
efo <efortin@users.noreply.github.com>
2019-11-29 05:58:08 +0100

    add Mac OSX build

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-GOOS=linux go build -o ./bin/kubi kubi-cli.go
-GOOS=windows GOARCH=386 go build -o ./bin/kubi.exe
-md5sum ./bin/kubi ./bin/kubi.exe > ./bin/md5
+GOOS=linux go build -o ./bin/linux/kubi kubi-cli.go
+GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s" -o ./bin/mac/kubi kubi-cli.go
+GOOS=windows GOARCH=386 go build -o ./bin/windows/kubi.exe
+md5sum ./bin/linux/kubi ./bin/windows/kubi.exe ./bin/mac/kubi > ./bin/md5
