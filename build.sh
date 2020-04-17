5a157c079258a4a80853bbcf7266f6be43638b52
Clément Blaise <clementblaise@me.com>
2020-04-17 11:11:19 +0200

    remove path from checksum and change algo to sha256

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 GOOS=linux go build -o ./bin/kubi kubi-cli.go
 GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s" -o ./bin/kubi-darwin kubi-cli.go
 GOOS=windows GOARCH=386 go build -o ./bin/kubi.exe
-md5sum ./bin/kubi ./bin/kubi.exe ./bin/kubi-darwin > ./bin/md5
+
+(cd ./bin && shasum -a 256 kubi kubi-darwin kubi.exe > sha256)
