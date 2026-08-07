5ea240f781766286e094fe5910e2dcf69e97d508
Kévin PEREZ <kevin.perez@ca-gip.fr>
2026-08-07 10:34:24 +0200

    typo

--- kubi-cli.go
+++ kubi-cli.go
@@ -39,7 +39,7 @@ func explainCmd(flagSet *flag.FlagSet) {
 	if len(flagSet.Args()) == 1 {
 		token = flagSet.Arg(0)
 		if len(strings.Split(token, ".")) != 3 {
-			internal.ExitIfError(fmt.Errorf("the token: %s is not a valid jwt token.\n", token))
+			internal.ExitIfError(fmt.Errorf("the token: %s is not a valid jwt token", token))
 		}
 	} else {
 		internal.LogLightGray("Using the kube config file for token explain")
